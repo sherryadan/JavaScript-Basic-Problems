bc663f7eedea02accd71f3ae99d4ed4fc072c788
sherryadan <adanmohammad80@gmail.com>
2023-07-27 10:09:55 -0700

    New Commit

--- problems.js
+++ problems.js
@@ -1,7 +1,7 @@
 //Problem no 1 Add a validation check to ensure that, a number is an integer. If it isn't, then issue an error message
-var number =4;
-function validateinteger(number){
-    if (!Number.isInteger){
+
+function validateinteger(Number){
+    if (!Number.isInteger(number)){
         console.log("Error: This is not an Integer")
     }
     else{
